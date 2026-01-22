e731ebd9d821a57351051e11808f2a678e9dee6f
Adilkhan <a_igisinov@kbtu.kz>
2026-01-22 11:55:19 +0500

    loading

---
+++ prac01/numbers/1.py
@@ -0,0 +1,34 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+
+print(random.randrange(1, 10))
+
