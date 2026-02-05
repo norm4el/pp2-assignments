65f972e8572d4568d399a9efdffd3067eee15a1d
Adilkhan <a_igisinov@kbtu.kz>
2026-02-05 11:45:50 +0500

    kaif

---
+++ prac03/classes/class_variables.py
@@ -0,0 +1,24 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil", 25)
+p1.greet()
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    return "Hello, " + self.name
+
+  def welcome(self):
+    message = self.greet()
+    print(message + "! Welcome to our website.")
+
+p1 = Person("Tobias")
+p1.welcome()
